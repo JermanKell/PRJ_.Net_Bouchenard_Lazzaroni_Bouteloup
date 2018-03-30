656a42410d21c87ddc8be983db2378871821f7c9
RBFUZ <remy.bouteloup@yahoo.fr>
2018-03-30 18:32:29 +0200

    Proposition 1 de gestion des évènements

---
+++ Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Controller
+    {
+        public event EventHandler<MyEventArgs> sendMessageToView;
+
+        public void parse()
+        {
+            MyEventArgs myArgs = new MyEventArgs();
+
+            myArgs.message = "Benjamin ???";
+            sendMessageToView(this, myArgs);
+
+            myArgs.message = "Préjjeennttt !!";
+            sendMessageToView(this, myArgs);
+
+            myArgs.message = "Raphael ?? ";
+            sendMessageToView(this, myArgs);
+
+            myArgs.message = "Préjjeennttt !!";
+            sendMessageToView(this, myArgs);
+
+            myArgs.message = "Xavier !!";
+            sendMessageToView(this, myArgs);
+
+            myArgs.message = "Préjjeennttt !!";
+            sendMessageToView(this, myArgs);
+        }
+    }
+
+    public class MyEventArgs : EventArgs
+    {
+        // Ici je peux mettre tous les attributs que je veux (string, int, double, ....).
+        // Un objet de cette classe est en paramètre de la méthode eventReceived de la vue.
+        // La vue à donc accès à l'ensemble des attributs de cette classe à chaque fois qu'elle recoit un évenement de la part du controleur.
+
+        public string message { get; set; }
+        public double pourcentage { get; set; }
+    }
+}
---
+++ View.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class View
+    {
+        public void clickOnButtonIntegration()
+        {
+            Controller controller = new Controller();
+            controller.sendMessageToView += eventReceived;
+            controller.parse();
+        }
+
+        static void eventReceived(object sender, MyEventArgs e)
+        {
+            // Ici, on vient ajouter le message à la vue, mettre à jour le pourcentage, etc ....
+            MessageBox.Show(e.message);
+        }
+    }
+}
