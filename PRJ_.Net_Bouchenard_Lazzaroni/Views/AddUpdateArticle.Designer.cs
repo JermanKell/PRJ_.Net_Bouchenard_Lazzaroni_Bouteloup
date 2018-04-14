accc61526dd2ed3f78619bbd2cc1dede8eb3a63e
RBFUZ <remy.bouteloup@yahoo.fr>
2018-04-14 12:03:21 +0200

    Changement design des fenêtres

--- PRJ_.Net_Bouchenard_Lazzaroni/Views/AddUpdateArticle.Designer.cs
+++ PRJ_.Net_Bouchenard_Lazzaroni/Views/AddUpdateArticle.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Lab_Title = new System.Windows.Forms.Label();
             this.Cbx_Marque = new System.Windows.Forms.ComboBox();
             this.Cbx_Famille = new System.Windows.Forms.ComboBox();
             this.Cbx_SousFamille = new System.Windows.Forms.ComboBox();
@@ -44,24 +43,16 @@
             this.Tbx_Prix = new System.Windows.Forms.TextBox();
             this.Tbx_Quantite = new System.Windows.Forms.TextBox();
             this.Btn_Valider = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // Lab_Title
-            // 
-            this.Lab_Title.AutoSize = true;
-            this.Lab_Title.Location = new System.Drawing.Point(204, 9);
-            this.Lab_Title.Name = "Lab_Title";
-            this.Lab_Title.Size = new System.Drawing.Size(11, 13);
-            this.Lab_Title.TabIndex = 0;
-            this.Lab_Title.Text = "*";
             // 
             // Cbx_Marque
             // 
             this.Cbx_Marque.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Cbx_Marque.FormattingEnabled = true;
-            this.Cbx_Marque.Location = new System.Drawing.Point(106, 281);
+            this.Cbx_Marque.Location = new System.Drawing.Point(155, 222);
             this.Cbx_Marque.Name = "Cbx_Marque";
-            this.Cbx_Marque.Size = new System.Drawing.Size(304, 21);
+            this.Cbx_Marque.Size = new System.Drawing.Size(235, 21);
             this.Cbx_Marque.TabIndex = 1;
             // 
             // Cbx_Famille
@@ -69,24 +60,24 @@
             this.Cbx_Famille.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Cbx_Famille.ForeColor = System.Drawing.SystemColors.WindowText;
             this.Cbx_Famille.FormattingEnabled = true;
-            this.Cbx_Famille.Location = new System.Drawing.Point(106, 196);
+            this.Cbx_Famille.Location = new System.Drawing.Point(155, 138);
             this.Cbx_Famille.Name = "Cbx_Famille";
-            this.Cbx_Famille.Size = new System.Drawing.Size(304, 21);
+            this.Cbx_Famille.Size = new System.Drawing.Size(235, 21);
             this.Cbx_Famille.TabIndex = 2;
             // 
             // Cbx_SousFamille
             // 
             this.Cbx_SousFamille.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.Cbx_SousFamille.FormattingEnabled = true;
-            this.Cbx_SousFamille.Location = new System.Drawing.Point(106, 239);
+            this.Cbx_SousFamille.Location = new System.Drawing.Point(155, 178);
             this.Cbx_SousFamille.Name = "Cbx_SousFamille";
-            this.Cbx_SousFamille.Size = new System.Drawing.Size(304, 21);
+            this.Cbx_SousFamille.Size = new System.Drawing.Size(235, 21);
             this.Cbx_SousFamille.TabIndex = 3;
             // 
             // Lab_Reference
             // 
             this.Lab_Reference.AutoSize = true;
-            this.Lab_Reference.Location = new System.Drawing.Point(86, 77);
+            this.Lab_Reference.Location = new System.Drawing.Point(17, 15);
             this.Lab_Reference.Name = "Lab_Reference";
             this.Lab_Reference.Size = new System.Drawing.Size(83, 13);
             this.Lab_Reference.TabIndex = 4;
@@ -95,7 +86,7 @@
             // Lab_Description
             // 
             this.Lab_Description.AutoSize = true;
-            this.Lab_Description.Location = new System.Drawing.Point(103, 101);
+            this.Lab_Description.Location = new System.Drawing.Point(17, 53);
             this.Lab_Description.Name = "Lab_Description";
             this.Lab_Description.Size = new System.Drawing.Size(66, 13);
             this.Lab_Description.TabIndex = 5;
@@ -104,7 +95,7 @@
             // Lab_Prix
             // 
             this.Lab_Prix.AutoSize = true;
-            this.Lab_Prix.Location = new System.Drawing.Point(102, 331);
+            this.Lab_Prix.Location = new System.Drawing.Point(17, 263);
             this.Lab_Prix.Name = "Lab_Prix";
             this.Lab_Prix.Size = new System.Drawing.Size(67, 13);
             this.Lab_Prix.TabIndex = 6;
@@ -113,7 +104,7 @@
             // Lab_Quantite
             // 
             this.Lab_Quantite.AutoSize = true;
-            this.Lab_Quantite.Location = new System.Drawing.Point(116, 366);
+            this.Lab_Quantite.Location = new System.Drawing.Point(17, 307);
             this.Lab_Quantite.Name = "Lab_Quantite";
             this.Lab_Quantite.Size = new System.Drawing.Size(53, 13);
             this.Lab_Quantite.TabIndex = 7;
@@ -121,14 +112,14 @@
             // 
             // Tbx_Reference
             // 
-            this.Tbx_Reference.Location = new System.Drawing.Point(175, 74);
+            this.Tbx_Reference.Location = new System.Drawing.Point(155, 12);
             this.Tbx_Reference.Name = "Tbx_Reference";
             this.Tbx_Reference.Size = new System.Drawing.Size(235, 20);
             this.Tbx_Reference.TabIndex = 8;
             // 
             // Tbx_Description
             // 
-            this.Tbx_Description.Location = new System.Drawing.Point(175, 101);
+            this.Tbx_Description.Location = new System.Drawing.Point(155, 50);
             this.Tbx_Description.Multiline = true;
             this.Tbx_Description.Name = "Tbx_Description";
             this.Tbx_Description.Size = new System.Drawing.Size(235, 72);
@@ -137,7 +128,7 @@
             // Lab_Famille
             // 
             this.Lab_Famille.AutoSize = true;
-            this.Lab_Famille.Location = new System.Drawing.Point(73, 180);
+            this.Lab_Famille.Location = new System.Drawing.Point(17, 138);
             this.Lab_Famille.Name = "Lab_Famille";
             this.Lab_Famille.Size = new System.Drawing.Size(97, 13);
             this.Lab_Famille.TabIndex = 10;
@@ -146,7 +137,7 @@
             // Lab_SousFamille
             // 
             this.Lab_SousFamille.AutoSize = true;
-            this.Lab_SousFamille.Location = new System.Drawing.Point(72, 223);
+            this.Lab_SousFamille.Location = new System.Drawing.Point(17, 178);
             this.Lab_SousFamille.Name = "Lab_SousFamille";
             this.Lab_SousFamille.Size = new System.Drawing.Size(122, 13);
             this.Lab_SousFamille.TabIndex = 11;
@@ -155,7 +146,7 @@
             // Lab_Marque
             // 
             this.Lab_Marque.AutoSize = true;
-            this.Lab_Marque.Location = new System.Drawing.Point(72, 265);
+            this.Lab_Marque.Location = new System.Drawing.Point(17, 222);
             this.Lab_Marque.Name = "Lab_Marque";
             this.Lab_Marque.Size = new System.Drawing.Size(103, 13);
             this.Lab_Marque.TabIndex = 12;
@@ -163,33 +154,44 @@
             // 
             // Tbx_Prix
             // 
-            this.Tbx_Prix.Location = new System.Drawing.Point(176, 328);
+            this.Tbx_Prix.Location = new System.Drawing.Point(155, 263);
             this.Tbx_Prix.Name = "Tbx_Prix";
-            this.Tbx_Prix.Size = new System.Drawing.Size(234, 20);
+            this.Tbx_Prix.Size = new System.Drawing.Size(235, 20);
             this.Tbx_Prix.TabIndex = 13;
             // 
             // Tbx_Quantite
             // 
-            this.Tbx_Quantite.Location = new System.Drawing.Point(175, 363);
+            this.Tbx_Quantite.Location = new System.Drawing.Point(155, 307);
             this.Tbx_Quantite.Name = "Tbx_Quantite";
             this.Tbx_Quantite.Size = new System.Drawing.Size(235, 20);
             this.Tbx_Quantite.TabIndex = 14;
             // 
             // Btn_Valider
             // 
-            this.Btn_Valider.Location = new System.Drawing.Point(150, 398);
+            this.Btn_Valider.Location = new System.Drawing.Point(289, 352);
             this.Btn_Valider.Name = "Btn_Valider";
-            this.Btn_Valider.Size = new System.Drawing.Size(157, 27);
+            this.Btn_Valider.Size = new System.Drawing.Size(101, 27);
             this.Btn_Valider.TabIndex = 15;
             this.Btn_Valider.Text = "*";
             this.Btn_Valider.UseVisualStyleBackColor = true;
             this.Btn_Valider.Click += new System.EventHandler(this.Btn_Valider_Click);
             // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(182, 352);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(101, 27);
+            this.cancelButton.TabIndex = 16;
+            this.cancelButton.Text = "Annuler";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
             // VueArticle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 437);
+            this.ClientSize = new System.Drawing.Size(415, 394);
+            this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.Btn_Valider);
             this.Controls.Add(this.Tbx_Quantite);
             this.Controls.Add(this.Tbx_Prix);
@@ -205,7 +207,6 @@
             this.Controls.Add(this.Cbx_SousFamille);
             this.Controls.Add(this.Cbx_Famille);
             this.Controls.Add(this.Cbx_Marque);
-            this.Controls.Add(this.Lab_Title);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "VueArticle";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
@@ -216,8 +217,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Lab_Title;
         private System.Windows.Forms.ComboBox Cbx_Marque;
         private System.Windows.Forms.ComboBox Cbx_Famille;
         private System.Windows.Forms.ComboBox Cbx_SousFamille;
@@ -233,5 +232,6 @@
         private System.Windows.Forms.TextBox Tbx_Prix;
         private System.Windows.Forms.TextBox Tbx_Quantite;
         private System.Windows.Forms.Button Btn_Valider;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
